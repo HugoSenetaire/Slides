fda0d61c789de5884ae4d2e12affd346d522f417
boombot01 <hugo.senetaire@eleves.enpc.fr>
2018-03-18 18:25:01 +0100

    Gestion des titres et des bullets en extractionTXT

--- Extraction-données-txt.py
+++ Extraction-données-txt.py
@@ -8,7 +8,7 @@ symbol = ["-", "*","."," "]
 titreTr=["Title","Titre"]
 titre1Tr=["Heading 1","Titre 1"]
 titre2Tr=["Heading 2","Titre 2"]
-titre3Tr=["Heading 3", "Titre 2"]
+titre3Tr=["Heading 3", "Titre 3"]
 Possibletitre= titreTr + titre1Tr + titre2Tr +titre3Tr
 soustitreTr=["Subtitle", "Sous-titre"]
 normalTr=["Normal", "Normal"]
@@ -44,14 +44,62 @@ def dataCutPlus(datapath):
         level = getTitleLevel(separate_words)
         if level > 0 :
             styles_list.append("Titre " + str(level)) #Titre de sous partie
+        elif len(separate_words) < 10 and words_list.index(words) == 0 :
+            styles_list.append("Titre") #Titre générale de la présentation
         else :
-            if len(separate_words) < 10 :
-                styles_list.append("Titre") #Titre générale de la présentation
+            if (words[0] in symbol):
+                index = words_list.index(words)
+                endBullet = False
+                while not endBullet :
+                    if words_list[index+1][0] in symbol :
+                        words+="\n"
+                        words+=words_list[index+1]
+                        print(words)
+                        
+                        words_list.remove(words_list[index+1])
+                    else :
+                        endBullet = True
+                words_list[index]=words
+                styles_list.append("Paragraphe de liste")
             else :
                 styles_list.append("Normal")
     doc.close()
     return(styles_list,words_list)
+
+def checkLevel(stylesList,structIndicator,ind): 
+    #Find the Level in which the slide is created (example  1-1-2 => [3,ind1,ind2,ind2])
+    # A factoriser !
+    assert(stylesList[ind] in Possibletitre)
+    if structIndicator[0]==0 :
+        while (stylesList[ind] in Possibletitre) :
+            structIndicator[0]+=1
+            structIndicator[structIndicator[0]] = ind
+            ind+=1
+        return structIndicator[structIndicator[0]] + 1
+    else :
+        if stylesList[ind][-1] == str(structIndicator[0]):
+            structIndicator[structIndicator[0]] = ind
+            ind+=1
+            while (stylesList[ind] in Possibletitre) :
+                structIndicator[0]+=1
+                structIndicator[structIndicator[0]] = ind
+                ind+=1
+            return structIndicator[structIndicator[0]] + 1
+        else :
+            structIndicator[0] =structIndicator[0] -1
+            return checkLevel(stylesList,structIndicator,ind)
+
     
+            
+            
+def titleGeneration(presentation,structIndicator,wordsList,slide):
+    assert(structIndicator[0]>0 and structIndicator[0] <4)
+    for i in range(1,structIndicator[0]+1) :
+        presentation.append(etree.SubElement(slide, "title"+str(i)))
+        index = structIndicator[i]
+
+        presentation[-1].text=(wordsList[index]).replace("\r"," ")
+        
     
 def dataCut(datapath):
     word=client.Dispatch("Word.Application")
@@ -84,10 +132,9 @@ def generateXMLTitle(presentation,slide,words_list):
     
     
 #Generation of the XML for a "Normal" text
-def generateXMLNormal(presentation,slide,words_list,ind,i):
+def generateXMLNormal(presentation,slide,words_list,structIndicator,i):
     slide.set("layout","title+paragraph")
-    presentation.append(etree.SubElement(slide, "title"))
-    presentation[-1].text=(words_list[ind]).replace("\r"," ")
+    titleGeneration(presentation,structIndicator,words_list,slide)
     presentation.append(etree.SubElement(slide,"paragraph"))
     listparagraphs=words_list[i].split("\r") #Séparation des paragraphes vis à vis retour ligne 
     presentation[-1].text=listparagraphs[0]
@@ -98,11 +145,10 @@ def generateXMLNormal(presentation,slide,words_list,ind,i):
     
     
 #Generation of the XML for a "List Pragraph" text
-def generateXMLBullets(presentation,slide,words_list,ind,i): 
+def generateXMLBullets(presentation,slide,words_list,structIndicator,i): 
     slide.set("layout","title+bullets")
-    presentation.append(etree.SubElement(slide, "title"))
-    presentation[-1].text=(words_list[ind]).replace("\r","")
-    listbullets=words_list[i].split("\r")
+    titleGeneration(presentation,structIndicator,words_list,slide)
+    listbullets=words_list[i].split("\n")
     for j in listbullets:
         if j!="":
             presentation.append(etree.SubElement(slide,"bullet"))
@@ -111,20 +157,18 @@ def generateXMLBullets(presentation,slide,words_list,ind,i):
 
 
 #Generation of the XML for an image ("Subtle emphasis" text)
-def generateXMLImage(presentation,slide,words_list,ind,i):
+def generateXMLImage(presentation,slide,words_list,structIndicator,i):
     slide.set("layout","title+paragraph+image")
-    presentation.append(etree.SubElement(slide, "title"))
-    presentation[-1].text=(words_list[ind]).replace("\r","")
+    titleGeneration(presentation,structIndicator,words_list,slide)
     presentation.append(etree.SubElement(slide,"image"))
     presentation[-1].text=(words_list[i].replace("\r",""))
 
 
 #Generation of the XML for a figure ("Strong" text)
-def generateXMLFigure(presentation,slide,words_list,ind,i):
+def generateXMLFigure(presentation,slide,words_list,structIndicator,i):
     if ":" in words_list[i]:
         slide.set("layout","title+figure+label")
-        presentation.append(etree.SubElement(slide, "title"))
-        presentation[-1].text=(words_list[ind]).replace("\r","")
+        titleGeneration(presentation,structIndicator,words_list,slide)
         textSplit=words_list[i].split(":")
         presentation.append(etree.SubElement(slide,"figure"))
         presentation[-1].text=(textSplit[0].replace("\r",""))
@@ -132,20 +176,26 @@ def generateXMLFigure(presentation,slide,words_list,ind,i):
         presentation[-1].text=(textSplit[1].replace("\r",""))
     else:
         slide.set("layout","title+figure")
-        presentation.append(etree.SubElement(slide, "title"))
-        presentation[-1].text=(words_list[ind]).replace("\r","")
+        titleGeneration(presentation,structIndicator,words_list,slide)
         presentation.append(etree.SubElement(slide,"figure"))
         presentation[-1].text=(words_list[i].replace("\r",""))  
 
 
 #Generation of the XML for a graph (tableau ?)
 def generateXMLgraph(presentation,slide,words_list,ind,i):
-    
+    return True
+
+def getIndex(list,element) :
+    if element in list :
+        return list.index(element)
+    else :
+        return +100000
 
 def xmlGeneration(datapath,output):
     language = 1
     #Gathering the data
-    styles_list,words_list=dataCut(datapath)
+    styles_list,words_list=dataCutPlus(datapath)
+    structIndicator=[0,-1,-1,-1]
     
     #Preparation of the xml file
     user = etree.Element("user")
@@ -155,7 +205,7 @@ def xmlGeneration(datapath,output):
     presentation=[]
     presentation.append(etree.SubElement(user, "diapo"))
     ind=0 #this variable will follow our progression on the word file
-    
+
     #Generation of the first slide. 2 possibilities : "Title" or "Title+Subtitle" slide
     slide=presentation[0]
     if styles_list[1]==soustitreTr[language]: 
@@ -165,32 +215,47 @@ def xmlGeneration(datapath,output):
         generateXMLTitle(presentation,slide,words_list)
         ind=1
     
-    #
+   
     while(not done):
         #Check if we reached the last heading
-        done=not (Possibletitre[language] in styles_list[ind+1:])
+
         
+        test = checkLevel(styles_list,structIndicator,ind) #Attention,Pourquoi in non modifié ?
+        ind = test
+        print("Primal ind")
+        print(ind)
+        done=not (titre1Tr[language] in styles_list[ind:] or \
+        titre2Tr[language] in styles_list[ind:] or \
+        titre3Tr[language] in styles_list[ind:])
+
         #Calculate the amount of slides for this heading
         if(not done):
-            final_ind=ind+1+styles_list[ind+1:].index(titre1Tr[language])
+            #auxInd = min(styles_list[ind:].index(titre1Tr[language]),styles_list[ind:].index(titre2Tr[language]),styles_list[ind:].index(titre3Tr[language]))
+            auxInd = min(getIndex(styles_list[ind:],titre1Tr[language]), \
+            getIndex(styles_list[ind:],titre2Tr[language]), \
+            getIndex(styles_list[ind:],titre3Tr[language]))
+            final_ind=ind+auxInd
         else:
             final_ind=len(styles_list)
+            print("nope")
+        print("Final Ind")
+        print(final_ind)
         
         #Iterate on the slide under this Heading
-        for i in range(ind+1,final_ind):
+        for i in range(ind,final_ind):
             presentation.append(etree.SubElement(user,"diapo"))
             slide=presentation[-1]
             if(styles_list[i]==normalTr[language]):
-                generateXMLNormal(presentation,slide,words_list,ind,i)
+                generateXMLNormal(presentation,slide,words_list,structIndicator,i)
                 
             elif(styles_list[i]==paraTr[language]):
-                generateXMLBullets(presentation,slide,words_list,ind,i)
+                generateXMLBullets(presentation,slide,words_list,structIndicator,i)
             elif(styles_list[i]==emphaseTr[language]):
-                generateXMLImage(presentation,slide,words_list,ind,i)
+                generateXMLImage(presentation,slide,words_list,structIndicator,i)
             elif(styles_list[i]==figureTr[language]):
-                generateXMLFigure(presentation,slide,words_list,ind,i)
+                generateXMLFigure(presentation,slide,words_list,structIndicator,i)
         ind=final_ind
-    
+
     #Save file
     filename=output
     with open(filename,'wb') as f:
