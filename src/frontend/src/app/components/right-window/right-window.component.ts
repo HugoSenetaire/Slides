d4667a440c16a995efd5751e625cdc61e5e8e72b
Diiso <louismontaut@gmail.com>
2018-05-13 22:49:33 +0200

    partie droite

--- src/frontend/src/app/components/right-window/right-window.component.ts
+++ src/frontend/src/app/components/right-window/right-window.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./right-window.component.css']
 })
 export class RightWindowComponent implements OnInit {
-
+  i:number = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  compile(){
+    var beamerHeight = document.getElementById("beamer1").height;
+    console.log(beamerHeight);
+    beamerHeight = 0;
+    console.log(beamerHeight);
+    this.i +=1
+    if (this.i==2){
+      var beamerHeight2 = document.getElementById("beamer2").height;
+      beamerHeight2 = 0;
+      console.log(beamerHeight2);
+    }
   }
 
 }
